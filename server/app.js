99add33570ba6d1e68715601378ae28f88fd8b82
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-11-20 02:01:42 +0000

    initialised app, express setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// imports
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+
+// initialisation
+const app = express();
+
+// middleware to handle GraphQL requests, at /graphql endpoint
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // schema
+  })
+);
+
+// port number + callback for log
+app.listen(4000, () => {
+  console.log('listening on port 4000 ...');
+});

d452d49b0f6c1604ba27c898f234266952abff24
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2019-11-21 13:46:50 +0000

    added author relation to Book

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -12,9 +12,9 @@ const {
 
 // temporary data while not connected to db
 let books = [
-  { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
-  { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
-  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
 ];
 
 let authors = [
@@ -29,7 +29,14 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        // console.log(parent);
+        return authors.find(author => author.id === parent.authorId);
+      }
+    }
   })
 });
 
